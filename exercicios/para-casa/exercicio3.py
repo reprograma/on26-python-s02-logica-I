91b727a57e94aaa68828acc882601c123c12fb9a
mariprs <mariana.pires.191@gmail.com>
2023-08-14 12:53:18 -0300

    Consertei errinhos

--- exercicios/para-casa/exercicio3.py
+++ exercicios/para-casa/exercicio3.py
@@ -7,7 +7,7 @@ print ("Digite o valor da sua conta de luz:")
 valor1 = (float(input()))
 print ("Digite o valor da sua conta de água:")
 valor2 = (float (input()))
-print ("Digite o valor da sua conta de interrnet:")
+print ("Digite o valor da sua conta de internet:")
 valor3 = (float (input()))
 
 print (f"No fim do mês, o valor total que você pagará é de: R${a_pagar_calc(valor1, valor2, valor3):.2f}")
