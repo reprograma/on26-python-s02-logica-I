35f6905bc0630cf4fd17c96c9733627e91020a36
Mariana Oliveira <141166198+Mariana-Ol@users.noreply.github.com>
2023-08-15 18:16:15 -0300

    Update exercicio-casa-2.py
    Fiz algumas alterações para o código ficar mais simples e renomeei 1 variável para o significado dela ficar óbvio.

--- exercicios/para-casa/exercicio-casa-2.py
+++ exercicios/para-casa/exercicio-casa-2.py
@@ -1,15 +1,14 @@
 area_total = float(input("Área a ser pintada em m²: "))
 
-a = (area_total/3)/18
+latas = (area_total/3)/18
 
-latas = round(a + 0.5)
+latas_arredondadas = round(a + 0.5)
 
-def valor_a_pagar(area_total, a, latas):
-    custo = latas*80
-    return custo
+custo = latas_arredondadas*80
 
-print("O número de latas necessário para pintar é:", latas)
-print("O valor a pagar é", valor_a_pagar(area_total, a, latas))
+
+print("O número de latas necessário para pintar é:", latas_arredondadas)
+print("O valor a pagar é", custo)
 
     
 
