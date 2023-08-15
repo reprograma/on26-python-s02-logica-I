381cba4b2a92f43b0b3ff82d3d9799e7d8c276e7
Belisa Arnhold <belisarenata@gmail.com>
2023-08-14 22:02:15 -0300

    exemplo da aula de revisão

--- exercicios/feitos-em-aula/exercicios-aula/funcoes-nota2.py
+++ exercicios/feitos-em-aula/exercicios-aula/funcoes-nota2.py
@@ -1,10 +1,31 @@
-def calcula_media_poderada(nota1, nota2, nota3, peso1, peso2, peso3):
+variavel_nova = 2
+
+def calcula_media_poderada(nota1, nota2, peso1, peso2, nota3=None, peso3=None):
     media = (nota1*peso1 + nota2*peso2 + nota3*peso3)/(peso1+peso2+peso3)
     return media
 
-nota1 = 15
-nota2 = 98
-nota3 = 19 
-peso1, peso2, peso3 =  float(input("peso1: ")), float(input("peso2: ")), float(input("peso3: "))
+x = 1
+y = 2
+z = 3 
+peso1 = 4
+peso2 = 5
+peso3 = 6
 
-print(calcula_media_poderada(nota1, nota2, nota3, peso1, peso2, peso3))
+retorno_da_funcao = calcula_media_poderada(nota1= x, nota2= 98, \
+                                           nota3 = z, peso1=  peso1, peso2 =peso2, peso3 = peso3)
+print(retorno_da_funcao)
+
+
+x = 7
+y = 8
+z = 9 
+peso1 = 4
+peso2 = 5
+peso3 = 6
+
+retorno_da_funcao = calcula_media_poderada(nota1= x, nota2= 98, \
+                                           nota3 = z, peso1=  peso1, peso2 =peso2, peso3 = peso3)
+
+
+print(retorno_da_funcao)
+#print(calcula_media_poderada(nota1, nota2, nota3, peso1, peso2, peso3))
