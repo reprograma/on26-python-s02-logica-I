af13827a97d63003b5d89dc48eee600e42288b10
danisnegrao <danisnegrao@gmail.com>
2023-08-16 17:59:36 -0300

    Nova Atividade Dani

---
+++ exercicios/ExercicioDani/funcao.py
@@ -0,0 +1,17 @@
+def soma():
+    print(a+b+c)
+
+a = float(input("digite a:",))
+b = float(input("digite b:",))
+c = float(input("digite c:",))
+
+
+soma()
+
+def funcao_quadratica():
+    print(x**x+x+5)
+
+
+
+x = float(input("Digite x:",)) 
+funcao_quadratica()
