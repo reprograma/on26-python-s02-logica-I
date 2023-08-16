a19dc76faf4cd673f4ffa0b6e386d1d0dbc23909
andreza oliveira <andrezarso@outlook.com>
2023-08-16 18:13:20 -0300

    Entrega exercicios semana 02

---
+++ exercicios/para-casa/exercicio03.py
@@ -0,0 +1,15 @@
+#Exercicio 03
+
+def soma(a,b,c):
+    res= a + b + c
+    return res
+
+print ('Resultado dos numeros')
+a = float(input('Primeiro numero: '))
+b = float(input('Segundo numero : '))
+c = float(input('Terceiro numero: '))
+
+print('Soma = ', soma(a,b,c))
+    
+
+
