ddbe6286e26a52ee409d7744792d2124e507f2f4
TayBoaes <taynaboaes@gmail.com>
2023-08-16 16:11:01 -0400

    Resolução Exercícios S02

---
+++ exercicios/para-casa/Questao_17_S02.py
@@ -0,0 +1,24 @@
+
+# Exercício 17 
+## https://wiki.python.org.br/EstruturaSequencial
+
+### Informações:
+### 1 litro para cada 6 metros quadrados 
+### A lata de 18 litros é vendida por R$ 80,00 ou em galões de 3,6 litros que custam R$ 25,00
+### Informar ao usuário a quantidade de tintas a serem compradas
+
+print ("Lojas de tintas")
+
+metros = float (input ("m2"))
+
+litros = 3 / metro_quadrado
+
+tamanho_da_lata_em_litros = 18
+
+preco_da_lata = 80
+
+print (Quantidade de tinta)
+
+print (Preco)
+
+
