84db42ee05951d4f4f78cf500a03f5ac546f5e3b
Lais Meireles Alves <141499800+laismeireles@users.noreply.github.com>
2023-08-15 22:37:35 -0300

    Add files via upload
    "exercício_16_versao_final"

--- exerc_16.py
+++ exerc_16.py
@@ -8,9 +8,32 @@ def calculo_quantidade (area_total, cobertura_tinta, volume_lata):
     quantidade = ((area_total/cobertura_tinta)/volume_lata)
     return(quantidade)
 
+def calculo_valor_da_compra (quantidade, valor_lata):
+    valor_da_compra = ((round(quantidade+0.5)*valor_lata))
+    return(valor_da_compra)
+
 area_total, cobertura_tinta, volume_lata = {}, 3.0, 18.0
 area_total = (float(input("Insira a área em m2 que será pintada: ")))
 quantidade = ((area_total/cobertura_tinta)/volume_lata)
-print("Você irá precisar de ")
-print(quantidade)
-print("latas")
+valor_lata = 80
+valor_da_compra = ((round(quantidade+0.5)*valor_lata))
+
+if quantidade == type(int):
+    print("Você irá precisar de ")
+    print(quantidade)
+    print("latas de tinta")
+else:
+    print("Você irá precisar de ")
+    print(round(quantidade+0.5))
+    print(" latas de tinta")
+
+print("E o valor total da compra será de ")
+print(valor_da_compra)
+print(" reais")
+
+
+
+
+
+    
+    
